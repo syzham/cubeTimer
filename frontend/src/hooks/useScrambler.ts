45f24789a62923fe5d588c348db0a3c790ef4c71
syzham <syzham3@gmail.com>
2024-12-30 15:05:24 +0700

    Change scrambler to generate state instead of random moves

--- frontend/src/hooks/useScrambler.ts
+++ frontend/src/hooks/useScrambler.ts
@@ -1,42 +1,77 @@
 import {useState} from "react";
-import {combineTwoMoves, inverse, possibleMoves} from "../utils/cubeUtils.ts";
+import * as cube from "../utils/cubeUtils.ts";
 
 export const useScrambler = () => {
-
-    const length = 25;
 
     const [ currScramble, setCurrScramble ] = useState<string[]>([]);
 
     const scramble = (): void => {
-        let face: number = 0;
-        const scram: string[] = []
-        while (scram.length < length) {
-            face = Math.floor(Math.random() * (possibleMoves.length));
-            if (scram.length > 0 && possibleMoves[face][0] == scram[scram.length - 1][0]) continue;
-            scram.push(possibleMoves[face]);
-        }
-        setCurrScramble(scram);
+        const newFacelet:string = generateFacelet();
+        cube.solve(newFacelet)
+            .then(data => {setCurrScramble(cube.inverseMoves(data.split(' ')))})
+            .catch(() => {setCurrScramble([])});
     };
 
     const getDisplayScramble = (currentMoves: string[]): string => {
         if (currScramble.length == 0) return '-';
         let redo: string = '';
-        let i = 0;
+        let i:number = 0;
         let anyMistakes: boolean = false;
 
         for (i; i < currentMoves.length; i++) {
             if (currentMoves[i] != currScramble[i]) {anyMistakes = true; break;}
         }
-        for (let j = currentMoves.length -1; j >= i; j--) {
+        for (let j:number = currentMoves.length -1; j >= i; j--) {
             if (!anyMistakes) continue;
-            if (j == currentMoves.length - 1) redo += combineTwoMoves(inverse(currentMoves[j]), currScramble[i]).join(' ') + ' ';
-            else redo += inverse(currentMoves[j]) + ' ';
+            if (j == currentMoves.length - 1) redo += cube.combineTwoMoves(cube.inverse(currentMoves[j]), currScramble[i]).join(' ') + ' ';
+            else redo += cube.inverse(currentMoves[j]) + ' ';
         }
         redo = (redo == '') ? currScramble[i] + ' ': redo;
 
         if (!anyMistakes && currentMoves.length == currScramble.length) return '';
 
         return redo + (currScramble.slice(i+1).join(' '));
+    }
+
+    const generateFacelet = (base:string = 'XXXXUXXXXXXXXRXXXXXXXXFXXXXXXXXDXXXXXXXXLXXXXXXXXBXXXX'): string => {
+        let newFacelet:string = base;
+        let used:number[] = [];
+        const clockWise:boolean[] = [true, false, true, true, false, false, true, false];
+        let i:number = 0;
+        for (const corner of cube.corners) {
+            let faces: [string, string, string];
+            [faces, used] = cube.availableCorners(base[corner[0]], base[corner[1]], base[corner[2]], used);
+            newFacelet =
+                newFacelet.substring(0, corner[0]) + faces[0] +
+                newFacelet.substring(corner[0]+1, corner[1]) + faces[clockWise[i] ? 1 : 2] +
+                newFacelet.substring(corner[1]+1, corner[2]) + faces[clockWise[i++] ? 2 : 1] +
+                newFacelet.substring(corner[2]+1);
+        }
+        while (!cube.validateCorners(newFacelet)) {
+            const corner:number[] = cube.corners[Math.floor(Math.random() * (cube.corners.length))];
+            newFacelet =
+                newFacelet.substring(0, corner[0]) + newFacelet[corner[1]] +
+                newFacelet.substring(corner[0]+1, corner[1]) + newFacelet[corner[2]] +
+                newFacelet.substring(corner[1]+1, corner[2]) + newFacelet[corner[0]] +
+                newFacelet.substring(corner[2]+1);
+        }
+        used = [];
+        for (const edge of cube.edges) {
+            let faces: [string, string];
+            [faces, used] = cube.availableEdges(base[edge[0]], base[edge[1]], used);
+            newFacelet =
+                newFacelet.substring(0, edge[0]) + faces[0] +
+                newFacelet.substring(edge[0]+1, edge[1]) + faces[1] +
+                newFacelet.substring(edge[1]+1);
+        }
+        if (!cube.validateEdges(newFacelet)) {
+            const edge:number[] = cube.edges[Math.floor(Math.random() * (cube.edges.length))];
+            newFacelet =
+                newFacelet.substring(0, edge[0]) + newFacelet[edge[1]] +
+                newFacelet.substring(edge[0]+1, edge[1]) + newFacelet[edge[0]] +
+                newFacelet.substring(edge[1]+1);
+        }
+        return newFacelet;
     }
 
     return { scramble, getDisplayScramble };
--- frontend/src/utils/cubeUtils.ts
+++ frontend/src/utils/cubeUtils.ts
@@ -22,7 +22,15 @@ export const inverse = (move: string): string => {
         return move + "'";
 }
 
-export const solve = async (moves: string) => {
+export const inverseMoves = (moves: string[]): string[] => {
+    const newMoves: string[] = [];
+    for (let i = moves.length; i >= 0 ; i--) {
+        newMoves.push(inverse(moves[i]));
+    }
+    return newMoves;
+}
+
+export const solve = async (moves: string): Promise<string> => {
     const response = await fetch('http://127.0.0.1:5000/solve', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
@@ -33,13 +41,109 @@ export const solve = async (moves: string) => {
         throw new Error(`HTTP error! Status: ${response.status}`);
     }
 
-    return await response.text(); // Parse JSON response
+    return await response.text();
 }
 
-export const possibleMoves: string[] = [
-    'U', 'D', 'B', 'F', 'L', 'R',
-    'U\'', 'D\'', 'B\'', 'F\'', 'L\'', 'R\'',
-    'U2', 'D2', 'B2', 'F2', 'L2', 'R2'
-];
+export const availableCorners = (color1:string, color2:string, color3:string, used:number[] = []):[[string, string, string], number[]] => {
+    const possibleCorners: string[][] =
+        [
+            ['U', 'L', 'B'],
+            ['U', 'B', 'R'],
+            ['U', 'F', 'L'],
+            ['U', 'R', 'F'],
+            ['R', 'D', 'F'],
+            ['R', 'B', 'D'],
+            ['F', 'D', 'L'],
+            ['D', 'B', 'L']
+        ]
+
+    let randomCorner:number = Math.floor(Math.random()*possibleCorners.length);
+    while (used.includes(randomCorner)) {
+        randomCorner = Math.floor(Math.random()*possibleCorners.length);
+    }
+    const randomRotation:number = Math.floor(Math.random()*3);
+
+    used.push(randomCorner);
+    return [[
+        possibleCorners[randomCorner][randomRotation],
+        possibleCorners[randomCorner][(1 + randomRotation)%3],
+        possibleCorners[randomCorner][(2 + randomRotation)%3]], used];
+}
+
+export const availableEdges = (color1:string, color2:string, used:number[] = []):[[string, string], number[]] => {
+    const possibleEdges: string[][] =
+        [
+            ['U', 'B'],
+            ['U', 'L'],
+            ['U', 'R'],
+            ['U', 'F'],
+            ['R', 'F'],
+            ['R', 'B'],
+            ['R', 'D'],
+            ['F', 'L'],
+            ['F', 'D'],
+            ['D', 'L'],
+            ['D', 'B'],
+            ['L', 'B']
+        ]
+    let randomEdge:number = Math.floor(Math.random()*possibleEdges.length);
+    while (used.includes(randomEdge)) {
+        randomEdge = Math.floor(Math.random()*possibleEdges.length);
+    }
+    const randomRotation:number = Math.floor(Math.random()*2);
+
+    used.push(randomEdge);
+    return [[
+        possibleEdges[randomEdge][randomRotation],
+        possibleEdges[randomEdge][(1 - randomRotation)]], used];
+}
+
+export const validateCorners = (facelet:string): boolean => {
+    const clockwise:number[] = [9, 17, 18, 26, 36, 44, 45, 53];
+    const counterClockwise:number[] = [11, 15, 20, 24, 38, 42, 47, 51];
+    let cornerPermutation:number = 0;
+    const UD:number[] = [...facelet.matchAll(/[UD]/g)].map(match => match.index);
+    cornerPermutation += UD.filter(value => clockwise.includes(value)).length;
+    cornerPermutation += UD.filter(value => (counterClockwise.includes(value))).length * 2;
+    return cornerPermutation%3 == 0;
+}
+
+export const validateEdges = (facelet:string): boolean => {
+    const rightEdges:(0|1)[] = [0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1];
+    let edgePermutation:number = 0;
+    for (let i = 0; i < edges.length; i++) {
+        if (facelet[edges[i][rightEdges[i]]] == 'U' || facelet[edges[i][rightEdges[i]]] == 'D') edgePermutation++;
+        else if (facelet[edges[i][1 - rightEdges[i]]] == 'L' || facelet[edges[i][1 - rightEdges[i]]] == 'R') edgePermutation++;
+    }
+    return edgePermutation%2 == 0;
+}
 
 export const solvedState = 'UUUUUUUUURRRRRRRRRFFFFFFFFFDDDDDDDDDLLLLLLLLLBBBBBBBBB';
+
+export const corners: number[][] =
+    [
+        [0, 36, 47],
+        [2, 11, 45],
+        [6, 18, 38],
+        [8, 9, 20],
+        [15, 26, 29],
+        [17, 35, 51],
+        [24, 27, 44],
+        [33, 42, 53]
+    ]
+
+export const edges: number[][] =
+    [
+        [1, 46],
+        [3, 37],
+        [5, 10],
+        [7, 19],
+        [12, 23],
+        [14, 48],
+        [16, 32],
+        [21, 41],
+        [25, 28],
+        [30, 43],
+        [34, 52],
+        [39, 50]
+    ]
